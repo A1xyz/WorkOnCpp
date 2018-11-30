efe7eabac1276b15d34e863e585afceaf23e94f8
PrabinaKumar <pradhan.prabian12@gmail.com>
2018-12-01 00:46:03 +0530

    Singleton Class file added with fun changes.

--- Singleton.cpp
+++ Singleton.cpp
@@ -8,6 +8,22 @@ class Singleton{
        Singleton() { data = 0; }
        ~Singleton() { }
    public:
+       void setdata(int val){
+           data = val;
+       }
+       int getdata(){
+           return data;
+       }
+       static Singleton* createInst(){
+           if( inst == NULL){
+               inst = new Singleton;
+           }
+           return inst;
+       }
+       static void destroy(){
+            delete inst; 
+            inst = NULL;
+       }
 
 };
 Singleton *Singleton::inst = NULL;
